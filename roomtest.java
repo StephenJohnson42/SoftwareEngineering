4f3835a4f8a3443e23f5a771b8bee6e757b5a296
WestBP <55105272+WestBP@users.noreply.github.com>
2019-10-14 14:26:32 -0400

    Added accessor methods to floorgen
    Added accessor methods to FloorGen class and respective tests to roomtest
    
    also documented the floorGen class a little more

--- roomtest.java
+++ roomtest.java
@@ -6,7 +6,17 @@ public static void main(String[] args) {
 
 	FloorGen test_floor = new FloorGen(); 
 	test_floor.Generate();
+	//The draw function is just for testing purposes
 	test_floor.draw();
+	
+	System.out.println("*****");
+	System.out.println("The starting room is: "+test_floor.get_starting_x()+","+test_floor.get_starting_y());
+	System.out.println("Room 2,2 contains downstairs: "+test_floor.check_down_stair(2,2));
+	System.out.println("Room 2,2 has a room to the north: "+test_floor.check_door_north(2,2));
+	System.out.println("Room 2,2 has a room to the south: "+test_floor.check_door_south(2,2));
+	System.out.println("Room 2,2 has a room to the east: "+test_floor.check_door_east(2,2));
+	System.out.println("Room 2,2 has a room to the west: "+test_floor.check_door_west(2,2));
+	System.out.println("*****");
 }
 
 }
