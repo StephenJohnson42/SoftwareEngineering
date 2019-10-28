6ffb1597722010758edca29a69e6a2fe1889fdab
bgressel <54748739+bgressel@users.noreply.github.com>
2019-10-28 16:35:23 -0400

    Update beneaththemanor.java

--- beneaththemanor.java
+++ beneaththemanor.java
@@ -9,24 +9,23 @@ import java.lang.String;
 
 public class beneaththemanor extends JFrame
 {
-	//setting all the icons
 	public Icon stones = new ImageIcon(getClass().getResource("stones.png"));
 	public Icon blackspace = new ImageIcon(getClass().getResource("black.png"));
-	public Icon arrowR = new ImageIcon(getClass().getResource("arrowR.png"));
-	public Icon arrowU = new ImageIcon(getClass().getResource("arrowU.png"));
 	public Icon character = new ImageIcon(getClass().getResource("character.png"));
 	public Icon startpanel = new ImageIcon(getClass().getResource("start.png"));
+	public Icon stairs = new ImageIcon(getClass().getResource("stairs.png"));
+	public Icon wall = new ImageIcon(getClass().getResource("wall.png"));
 	public JButton[] track=new JButton[100];
 	public int[] nums=new int[100];
 	public int indoor;
-	public int outdoor;
-	
+	public int[] outdoor=new int[4]; //NSEW
     public startGame start=new startGame();
     public KeyListener mover;
     public int trackmover;
     public ActionListener initialclick;
-    int roomcounter;
     Random rand = new Random();
+    FloorGen floor;
+    int x, y;
     
     public beneaththemanor()
     {
@@ -41,7 +40,7 @@ public class beneaththemanor extends JFrame
             setLayout(new GridLayout(10,10,0,0));
             
            
-            //setting the individual spaces
+            
             for (int i=0; i<100; i++) {
                 track[i]=new JButton();
                 track[i].setIcon(stones);
@@ -61,15 +60,40 @@ public class beneaththemanor extends JFrame
             	}
                 
             }
+            floor = new FloorGen();
+            floor.Generate();
             
-            roomcounter=1;
-            trackmover=50;
-            indoor=trackmover;
-            outdoor=29;
-            nums[outdoor]=1;
-            track[outdoor].setIcon(arrowR);
+            outdoor[0]=0;
+            outdoor[1]=0;
+            outdoor[2]=0;
+            outdoor[3]=0;
             
-	    //setting the character
+            trackmover=45;
+            
+            x= floor.get_starting_x();
+            y= floor.get_starting_y();
+            
+            if (floor.check_door_north(x,y)) {
+            	outdoor[0]=1;
+                nums[5]=1;
+                track[5].setIcon(blackspace);
+            }
+            if (floor.check_door_east(x,y)) {
+            	outdoor[2]=1;
+                nums[59]=1;
+                track[59].setIcon(blackspace);
+            }
+            if (floor.check_door_west(x,y)) {
+            	outdoor[3]=1;
+                nums[50]=1;
+                track[50].setIcon(blackspace);
+            }
+            if (floor.check_door_south(x,y)) {
+            	outdoor[1]=1;
+                nums[95]=1;
+                track[95].setIcon(blackspace);
+            }
+            
             track[trackmover].setIcon(startpanel);
             initialclick =  new ActionListener(){
                 public void actionPerformed(ActionEvent event)
@@ -79,7 +103,6 @@ public class beneaththemanor extends JFrame
             };
             track[trackmover].addActionListener(initialclick); 
             
-	    //the code that allows the user to move the character
             mover =  new KeyListener(){
             	@Override
                 public void keyPressed(KeyEvent e)
@@ -92,8 +115,8 @@ public class beneaththemanor extends JFrame
             		
             	    if (key == KeyEvent.VK_LEFT) {
             	    	x=nums[trackmover-1];
-            	    	if ((trackmover-1)==outdoor){
-            	    		newRoom();
+            	    	if ((trackmover-1)==50 && outdoor[3]==1){
+            	    		newRoom(50);
             	    	}
             	    	else if (x==1) {
             	    	track[trackmover].setIcon(blackspace);
@@ -104,8 +127,8 @@ public class beneaththemanor extends JFrame
 
             	    if (key == KeyEvent.VK_RIGHT) {
             	    	x=nums[trackmover+1];
-            	    	if ((trackmover+1)==outdoor){
-            	    		newRoom();
+            	    	if ((trackmover+1)==59 && outdoor[2]==1){
+            	    		newRoom(59);
             	    	}
             	    	else if (x==1) {
             	    	track[trackmover].setIcon(blackspace);
@@ -116,8 +139,11 @@ public class beneaththemanor extends JFrame
 
             	    if (key == KeyEvent.VK_UP) {
             	    	x=nums[trackmover-10];
-            	    	if ((trackmover-10)==outdoor){
-            	    		newRoom();
+            	    	if ((trackmover-10)==5 && outdoor[0]==1){
+            	    		newRoom(5);
+            	    	}
+            	    	else if (nums[1]==1 && (trackmover-10)==1) {
+            	    		newFloor();
             	    	}
             	    	else if (x==1) {
             	    	track[trackmover].setIcon(blackspace);
@@ -128,8 +154,8 @@ public class beneaththemanor extends JFrame
 
             	    if (key == KeyEvent.VK_DOWN) {
             	    	x=nums[trackmover+10];
-            	    	if ((trackmover+10)==outdoor){
-            	    		newRoom();
+            	    	if ((trackmover+10)==95 && outdoor[1]==1){
+            	    		newRoom(95);
             	    	}
             	    	else if (x==1) {
             	    	track[trackmover].setIcon(blackspace);
@@ -155,8 +181,8 @@ public class beneaththemanor extends JFrame
  
             }
         
-	//generates a new room based on the exit of the previous room
-        public void newRoom(){
+        public void newRoom(int previousRoom){
+        	
         	for (int i=0; i<100; i++) {
                 track[i].setIcon(stones);
                 nums[i]=0;
@@ -174,30 +200,129 @@ public class beneaththemanor extends JFrame
             	}
             }
             
-           if (roomcounter==0) {
-        	   indoor=outdoor+90;
-               trackmover=indoor;
-            	outdoor=((rand.nextInt(8)+1)*10+9); 
-            	roomcounter=1;
-            	track[outdoor].setIcon(arrowR);
-            	
-           }
             
-            else if (roomcounter==1) {
-            	indoor=outdoor-9;
-            	trackmover=indoor;
-            	
-            	outdoor=((rand.nextInt(8)+1)); 
-            	roomcounter=0;
-            	track[outdoor].setIcon(arrowU);
-            	
+         
+            if (previousRoom==5) {
+            	y++;
+        	   indoor=95;
+        	   nums[indoor]=1;
+               trackmover=85;
+               
             }
-           track[indoor].setIcon(character);
-           
-            nums[outdoor]=1;
+            else if (previousRoom==95) {
+            	y--;
+            	indoor=5;
+            	nums[indoor]=1;
+            	trackmover=15;
+            }
+            else if (previousRoom==50) {
+            	x--;
+            	indoor=59;
+            	nums[indoor]=1;
+            	trackmover=58;
+            }
+            else {
+            	x++;
+            	indoor=50;
+            	nums[indoor]=1;
+            	trackmover=51;
+            }
             
+            
+            
+            outdoor[0]=0;
+            outdoor[1]=0;
+            outdoor[2]=0;
+            outdoor[3]=0;
+            
+            if (floor.check_door_north(x,y)) {
+            	outdoor[0]=1;
+                nums[5]=1;
+                track[5].setIcon(blackspace);
+            }
+            if (floor.check_door_east(x,y)) {
+            	outdoor[2]=1;
+                nums[59]=1;
+                track[59].setIcon(blackspace);
+            }
+            if (floor.check_door_west(x,y)) {
+            	outdoor[3]=1;
+                nums[50]=1;
+                track[50].setIcon(blackspace);
+            }
+            if (floor.check_door_south(x,y)) {
+            	outdoor[1]=1;
+                nums[95]=1;
+                track[95].setIcon(blackspace);
+            }
+            
+            if (floor.check_down_stair(x, y)) {
+            	nums[1]=1;
+            	track[1].setIcon(stairs);
+            }
+            track[trackmover].setIcon(character);
+            
+        }
+        
+        public void newFloor(){
+        	
+        	floor.Generate();
+        	
+        	for (int i=0; i<100; i++) {
+                track[i].setIcon(stones);
+                nums[i]=0;
+            }
+            int temp=0;
+            for (int i=11; i<90; i++) {
+            	if (temp!=8) {
+            		track[i].setIcon(blackspace);
+            		temp++;
+            		nums[i]=1;
+            	}
+            	else {
+            		temp=0;
+            		i++;
+            	}
+            }
+            
+            outdoor[0]=0;
+            outdoor[1]=0;
+            outdoor[2]=0;
+            outdoor[3]=0;
+            
+            trackmover=81;
+            
+            x= floor.get_starting_x();
+            y= floor.get_starting_y();
+            
+            if (floor.check_door_north(x,y)) {
+            	outdoor[0]=1;
+                nums[5]=1;
+                track[5].setIcon(blackspace);
+            }
+            if (floor.check_door_east(x,y)) {
+            	outdoor[2]=1;
+                nums[59]=1;
+                track[59].setIcon(blackspace);
+            }
+            if (floor.check_door_west(x,y)) {
+            	outdoor[3]=1;
+                nums[50]=1;
+                track[50].setIcon(blackspace);
+            }
+            if (floor.check_door_south(x,y)) {
+            	outdoor[1]=1;
+                nums[95]=1;
+                track[95].setIcon(blackspace);
+            }
+            
+            track[91].setIcon(wall);
+            track[trackmover].setIcon(character);
             
         }
         
         }
+    
+    
     }
+
