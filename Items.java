1b290004e38288c948df71457e6228d11c904f63
StephenJohnson42 <54747618+StephenJohnson42@users.noreply.github.com>
2019-11-03 08:41:01 -0500

    Add possible item strings and number generation private int

--- Items.java
+++ Items.java
@@ -5,6 +5,12 @@ import java.lang.Integer;
 import java.lang.String;
 
 public class Items{
+  private int itemNum;
+  private String[] itemImg = {"item0.png", "item1.png", "item2.png", "item3.png", "item4.png", "item5.png", "item6.png", 
+                              "item7.png", "item8.png", "item9.png"};
   public Items(){
+      Random rand = new Random(); //Create random number generator
+      itemNum = rand.nextInt(10); //Returns a random int between 0 and 9
+                                  //Random numbers will be however many items we want
   }
 }
